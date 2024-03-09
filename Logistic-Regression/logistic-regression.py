46740934d0ac594a2638ae69bd6f72617b536896
U220717026 <elasemrasava@posta.mu.edu.tr>
2024-03-09 19:26:58 +0300

    confusion matrix added.

--- Logistic-Regression/logistic-regression.py
+++ Logistic-Regression/logistic-regression.py
@@ -5,13 +5,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 
-datas = pd.read_csv("data.csv")
+datas = pd.read_csv("Logistic-Regression/data.csv")
 x = datas.iloc[:,1:4].values #independent variables
 y = datas.iloc[:,4:].values #dependent variable
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0) #split the data into test and train
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0) #split the data into test and train
 
 sc = StandardScaler()
 X_train = sc.fit_transform(x_train)
@@ -24,3 +25,6 @@ y_pred = logr.predict(X_test)
 
 print(y_pred)
 print(y_test)
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
